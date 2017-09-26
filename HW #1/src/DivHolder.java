ce5544cd54887e1acc831ab8efe130a696ea685b
Alex <Alex@DESKTOP-MVK1R9S>
2017-09-26 18:48:30 -0400

    Final

--- HW #1/src/DivHolder.java
+++ HW #1/src/DivHolder.java
@@ -1,27 +1,128 @@
-
+/*
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * This class is just for holding values and doing computations
+ */
 public class DivHolder {
 	private int startIdx;
 	private int endIdx;
-	private int lineNmr;
 	private String content;
-	
-public DivHolder(int startindex, int endindex,int linenumber, String info) {
+	private static String noun = "no word provided";
+	private static String verb = "no word provided";
+	private static String adj = "no word provided";
+	/*
+	 * @author WinstanleyA
+     * @Date 9/26/17
+	 * @param none
+	 */
+public DivHolder() {	
+	startIdx = -1;
+	endIdx = -1;
+	content ="null";
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @param startindex is the index of <
+ * @param endindex is the index of >
+ * @param info is the word inside <>
+ */
+public DivHolder(int startindex, int endindex, String info) {
 	startIdx = startindex;
 	endIdx = endindex;
-	lineNmr = linenumber;
-	content = info;
-	
+	if(info.equals("noun"))
+	content = noun;
+	else if(info.equals("verb"))
+	content = verb;
+	else if(info.equals("adjective"))
+	content = adj;
+	else
+	content = "please add " + info + " to the static fields";
 }
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @param original is the String you wish to modify
+ * @return output is the modified version that has replaced all <> with one of the three
+ * word choices
+ */
+public String replace(String original) {
+	String output = "";
+	output+=original.substring(0, startIdx);
+	output+=content;
+	output+=original.substring(endIdx+1);
+	return output;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @return startIdx the private variable startIdx
+ */
 public int getStartIdx() {
 	return startIdx;
 }
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @return endIdx the private variable endIdx
+ */
 public int getEndIdx() {
 	return endIdx;
 }
-public int getlineNmr() {
-	return lineNmr;
-}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @return content the private variable content
+ */
 public String getContent() {
 	return content;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @return noun the private variable noun
+ */
+public String getNoun() {
+	return noun;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @return verb the private variable verb
+ */
+public String getVerb() {
+	return verb;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @return adj the private variable adj
+ */
+public String getAdj() {
+	return adj;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @param in the word you want to set verb equal to 
+ */
+public void setVerb(String in) {
+	verb= in;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @param in the word you want to set verb noun to 
+ */
+public void setNoun(String in) {
+	noun= in;
+}
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ * @param in the word you want to set adj equal to 
+ */
+public void setAdj(String in) {
+	adj= in;
 }
 }
--- HW #1/src/IOMain.java
+++ HW #1/src/IOMain.java
@@ -1,3 +1,7 @@
+/* 
+ * @author WinstanleyA
+ * @Date 9/26/17
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -5,7 +9,12 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class IOMain {
-	
+	/* 
+	 * @author WinstanleyA
+	 * @Date 9/26/17
+	 * @param fNme the name of the file your writing to
+	 * @return input a PrintWriter which will be able to write to the file given in fNme
+	 */
 	public static PrintWriter readerW(String fNme) {
 		File scannable = new File( fNme );
 		PrintWriter input = null;
@@ -17,6 +26,12 @@ public class IOMain {
 		}
 		return input;
 	}
+	/* 
+	 * @author WinstanleyA
+	 * @Date 9/26/17
+	 * @param fNme the name of the file you wish to read
+	 * @return input a Scanner to read the file you gave as input
+	 */
 		public static Scanner reader(String fNme) {
 		File scannable = new File( fNme );
 		Scanner input = null;
@@ -27,6 +42,12 @@ public class IOMain {
 		}
 		return input;
 	}
+		/* 
+		 * @author WinstanleyA
+		 * @Date 9/26/17
+		 * @param input is the scanner of the file you want to check if braces are balanced
+		 * @param output is the print writer you want to output the answer to
+		 */
 		public static void writeJava (Scanner input, PrintWriter output) {
 			int counts= 0;
 			while(input.hasNextLine()) {
@@ -49,6 +70,13 @@ public class IOMain {
 				output.println("Braces Not Balanced");
 			output.println("");
 		}
+		/* 
+		 * @author WinstanleyA
+		 * @Date 9/26/17
+		 * @param input is the Scanner of the first file you want to compare
+		 * @param input2 is the Scanner of the Second file you want to compare
+		 * @param output is the print writer of the file you wish to send the answer to
+		 */
 		public static void checkCode(Scanner input, Scanner input2, PrintWriter output) {
 			while(input.hasNextLine()) {
 				
@@ -65,35 +93,42 @@ public class IOMain {
 			output.println("Files Identical");
 			output.println("");
 		}
-		public static void revision(Scanner input, PrintWriter output, Scanner noun, Scanner adj, Scanner verb) {
-			ArrayList<DivHolder> divs = new ArrayList<DivHolder>();
-			ArrayList<String> lines = new ArrayList<String>();
+		/* 
+		 * @author WinstanleyA
+		 * @Date 9/26/17
+		 * @param input is the Scanner of the story you want to replace all the <> with words
+		 * @param output is the print writer of the file you want to print your revised story to
+		 */
+		public static void revision(Scanner input, PrintWriter output) {
 			while(input.hasNextLine()) {
-				lines.add(input.nextLine());
+				String tester = input.nextLine();
+				while(tester.indexOf("<")!=-1 ){
+				DivHolder newLine = new DivHolder(tester.indexOf("<"),tester.indexOf(">"),
+						tester.substring(tester.indexOf("<")+1, tester.indexOf(">")));
+					tester = newLine.replace(tester);	
+			  }
+				output.println(tester);
 			}
-			for(int i = 0;i<lines.size();i++) {
-				String tester = lines.get(i);
-			  do {
-				int first = (lines.get(i)).indexOf("<") ;
-				int last = (lines.get(i)).indexOf(">");
-				divs.add(new DivHolder(first, last, i, tester.substring(first, last)));
-				tester = tester.substring(last);
-			  }while(tester.indexOf("<")!= -1);			
-			}
-			for(int i =0; i<divs.size(); i++) {
-				DivHolder hold = divs.get(i);
-				String newLine = "";
-				
-				
-			}
+
 		}
-	
-		
+		/* 
+		 * @author WinstanleyA
+		 * @Date 9/26/17
+		 * @param args is the names of all the files you want to use in this program
+		 */
 		public static void main(String[] args) {
-			Scanner inNoun = new Scanner(System.in);
-			Scanner inAdj = new Scanner(System.in);
-			Scanner inVerb = new Scanner(System.in);
-			// TODO Auto-generated method stub
+			ArrayList<String> word = new ArrayList<String>();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("please input an noun");
+			word.add(sc.next());
+			System.out.println("please input an adjective");
+			word.add(sc.next());
+			System.out.println("please input a verb");
+			word.add(sc.next());
+			DivHolder control = new DivHolder();
+			control.setNoun(word.get(0));
+			control.setAdj(word.get(1));
+			control.setVerb(word.get(2)); 
 			Scanner in = reader(args[0]);
 			Scanner inTooDeep = reader(args[2]);
 			Scanner imNotAnAuthor = reader(args[3]);
@@ -112,9 +147,10 @@ public class IOMain {
 				}
 			writeJava(in, out);
 			checkCode(in, inTooDeep, out);
-			revision(imNotAnAuthor, out, inNoun, inAdj, inVerb);
+			revision(imNotAnAuthor, out);
 			in.close(); 
 			out.close();
+			sc.close();
 		}
 
 }
